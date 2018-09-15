4ec808fa488448a010b46721f8455b7cf99fd9ca
Yuan Zhang <z5122988@ad.unsw.edu.au>
2018-09-15 11:33:30 +1000

    Test each new shape

--- MTRN2500_ass2/Shape.hpp
+++ MTRN2500_ass2/Shape.hpp
@@ -1,7 +1,21 @@
 
 #ifndef MTRN3500_SHAPE_H
 #define MTRN3500_SHAPE_H
+class Coordinate {
+public:
+	Coordinate() = default;
+	~Coordinate() = default;
+	Coordinate(double x, double y) :x(x), y(y),z(z) {}
+	double GetX() { return this->x; }
+	double GetY() { return this->y; }
+	double GetZ() { return this->z; }
+	void Set(double x, double y, double z) { this->x = x, this->y, this->z = z; }
 
+private:
+	double x;
+	double y;
+	double z;
+};
 // Abstract class
 class Shape {
 
@@ -33,11 +47,37 @@ public:
 	double getBlue();
 	void setColor(float red_, float green_, float blue_);
 
+
+
 protected:
 	double x, y, z;               // position
 	double rotation;              // heading of the object in the horizontal plane (degrees)
 	float red, green, blue;       // colour of object
 
+
 };
+
+class Rectprism : public Shape
+{
+public:
+	void setrectprism(double base_, double height_, double length_);
+	virtual void draw();
+protected:
+	double base, height, length;
+
+};
+class Triprism : public Shape
+{
+public:
+	void setcoordiniate(Coordinate one, Coordinate two, Coordinate three);
+	void setheight(double height_);
+	virtual void draw();
+	
+protected:
+	Coordinate First_p, Second_p, Third_p;
+	double height;
+};
+
+
 
 #endif // for MTRN3500_SHAPE_H
--- MTRN2500_ass2/main.cpp
+++ MTRN2500_ass2/main.cpp
@@ -131,7 +131,24 @@ int main(int argc, char ** argv) {
 
 	return 0;
 }
+void draw_rect_prism(double x, double y, double z, double rotation,double length,double base,double height) {
+	Rectprism instance;
+	instance.setPosition(x, y, z);
+	instance.setRotation(rotation);
+	instance.setrectprism(base,height,length);
+	instance.draw();
+}
+void draw_tri_prism(double x, double y, double z, double rotation, Coordinate one, Coordinate two, Coordinate three,double height) 
+{
+	Triprism instance;
+	/*instance.setPosition(x, y, z);
+	instance.setcoordiniate(one, two, three);
+	instance.setRotation(rotation);
+	instance.setheight(height);*/
+	instance.draw();
 
+
+}
 void drawGoals()
 {
 	for (int i = 0; i < goals.size(); ++i) {
@@ -194,6 +211,19 @@ void display() {
 
 	// draw HUD
 	HUD::Draw();
+
+	Coordinate one, two, three;
+	one.Set(3, 1, 3);
+	two.Set(-3, 2, 3);
+	three.Set(0, 3, 0);
+
+	//draw_rect_prism(0, 0, 0, 0, 10, 10, 10);
+
+	draw_tri_prism(0, 0, 0, 0, one,two,three,10);
+
+	
+
+
 
 	glutSwapBuffers();
 };
