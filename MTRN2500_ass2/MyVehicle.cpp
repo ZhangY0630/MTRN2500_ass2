54643b3c5bd5b6e479eedbb5d50e9dcae2f6e1ee
EricLi17 <43270523+EricLi17@users.noreply.github.com>
2018-09-15 15:36:04 +1000

    added all wheels , head and tail

--- MTRN2500_ass2/MyVehicle.cpp
+++ MTRN2500_ass2/MyVehicle.cpp
@@ -56,7 +56,53 @@ void MyVehicle::SetLocalmd()//upload to server
 	CylA1.rotation = 0;
 	Localmd.shapes.push_back(CylA1);
 	
+	//upload cylinderA2 front left
+	ShapeInit CylA2;
+	CylA2.type = CYLINDER;
+	CylA2.rgb[0] = 1;
+	CylA2.rgb[1] = 0.4;
+	CylA2.rgb[2] = 0;
+	CylA2.xyz[0] = 1.5;//from message
+	CylA2.xyz[1] = 0;
+	CylA2.xyz[2] = -1.1;
+	CylA2.params.cyl.radius = 0.4;//from messages
+	CylA2.params.cyl.depth = 0.3;
+	CylA2.params.cyl.isRolling = 1;
+	CylA2.params.cyl.isSteering = 1;
+	CylA2.rotation = 0;
+	Localmd.shapes.push_back(CylA2);
 
+	//upload cylinderA3 back right 
+	ShapeInit CylA3;
+	CylA3.type = CYLINDER;
+	CylA3.rgb[0] = 0;
+	CylA3.rgb[1] = 0;
+	CylA3.rgb[2] = 1;
+	CylA3.xyz[0] = -1.5;//from message
+	CylA3.xyz[1] = 0;
+	CylA3.xyz[2] = 1.1;
+	CylA3.params.cyl.radius = 0.8;//from messages
+	CylA3.params.cyl.depth = 0.3;
+	CylA3.params.cyl.isRolling = 1;
+	CylA3.params.cyl.isSteering = 0;
+	CylA3.rotation = 0;
+	Localmd.shapes.push_back(CylA3);
+
+	//upload cylinderA4 back left
+	ShapeInit CylA4;
+	CylA4.type = CYLINDER;
+	CylA4.rgb[0] = 0;
+	CylA4.rgb[1] = 0;
+	CylA4.rgb[2] = 1;
+	CylA4.xyz[0] = -1.5;//from message
+	CylA4.xyz[1] = 0;
+	CylA4.xyz[2] = -1.1;
+	CylA4.params.cyl.radius = 0.8;//from messages
+	CylA4.params.cyl.depth = 0.3;
+	CylA4.params.cyl.isRolling = 1;
+	CylA4.params.cyl.isSteering = 0;
+	CylA4.rotation = 0;
+	Localmd.shapes.push_back(CylA4);
 	
 	//upload rectangle,it is a package
 	ShapeInit Rectbody;
@@ -73,7 +119,38 @@ void MyVehicle::SetLocalmd()//upload to server
 	Rectbody.rotation = 0;
 	Localmd.shapes.push_back(Rectbody);
 
+	//upload trianglePrism
+	ShapeInit trighead;
+	trighead.type = TRIANGULAR_PRISM;
+	trighead.rgb[0] = 1;
+	trighead.rgb[1] = 0;
+	trighead.rgb[2] = 0;
+	trighead.xyz[0] = 2.5;//from message
+	trighead.xyz[1] = 0.4;
+	trighead.xyz[2] = 0;
+	trighead.params.tri.alen = 2;//from messages
+	trighead.params.tri.blen = 1;
+	trighead.params.tri.angle = 90;
+	trighead.params.tri.depth = 2;
+	trighead.rotation = 0;
+	Localmd.shapes.push_back(trighead);
 
+	//upload trapezoidalPrism
+	ShapeInit traptail;
+	traptail.type = TRAPEZOIDAL_PRISM;
+	traptail.rgb[0] = 0.8;
+	traptail.rgb[1] = 0;
+	traptail.rgb[2] = 0;
+	traptail.xyz[0] = -2.5;//from message
+	traptail.xyz[1] = 0.4;
+	traptail.xyz[2] = 0;
+	traptail.params.trap.alen = 2;//from messages
+	traptail.params.trap.blen = 3;
+	traptail.params.trap.height = 1;
+	traptail.params.trap.aoff = 0;
+	traptail.params.trap.depth = 2;
+	traptail.rotation = 180;
+	Localmd.shapes.push_back(traptail);
 }
 
 VehicleModel MyVehicle::getremote()
